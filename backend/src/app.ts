21c5f100e7036511769b9cd8991388cdf8757ed9
Adao-Angelo <adaobegginer@gmail.com>
2024-11-10 13:59:20 +0100

    creata chat connections

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,17 +4,22 @@ import morgan from "morgan";
 import cors from "cors";
 import routes from "./routes";
 import { errorHandler } from "./middlewares";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const app = express();
 
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
-
 app.use(routes);
-
 app.use(errorHandler);
 
-export default (): express.Application => {
-  return app;
+const httpServer = createServer(app);
+const io = new Server(httpServer, {});
+
+export { io };
+
+export default () => {
+  return httpServer;
 };
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,7 @@ import app from "./app";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.API_PORT || 3000;
 
 const server = app();
 
